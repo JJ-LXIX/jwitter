cdf9bb6234b5950151080f7779158e5ee76125d3
Jagrath <jagrathshettylxix@gmail.com>
2023-03-27 14:12:42 +0530

    commented Modal component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,15 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Layout from "../Components/Layout";
+import Modal from "../Components/Modal";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      {/* <Modal isOpen title="test modal" /> */}
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
