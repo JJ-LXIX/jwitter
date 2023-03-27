5784281b59fbb180fb59f4e50c235009dd5f9e2e
Jagrath <jagrathshettylxix@gmail.com>
2023-03-27 20:59:33 +0530

    added login and register modal components to _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,15 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Layout from "../Components/Layout";
 import Modal from "../Components/Modal";
+import LoginModal from "../Components/modals/LoginModal";
+import RegisterModal from "../Components/modals/RegisterModal";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       {/* <Modal isOpen title="test modal" /> */}
+      <LoginModal />
+      <RegisterModal />
       <Layout>
         <Component {...pageProps} />
       </Layout>
