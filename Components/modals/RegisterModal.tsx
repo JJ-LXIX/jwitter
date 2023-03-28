af0e3dbf6acfecdcd29629ce48fde6adb5b112ca
Jagrath <jagrathshettylxix@gmail.com>
2023-03-28 11:53:14 +0530

    added register functionality to modal

--- Components/modals/RegisterModal.tsx
+++ Components/modals/RegisterModal.tsx
@@ -1,8 +1,11 @@
 import { useCallback, useState } from "react";
+import axios from "axios";
 import useLoginModal from "../../hooks/useLoginModal";
 import useRegisterModal from "../../hooks/useRegisterModal";
 import Input from "../Input";
 import Modal from "../Modal";
+import { toast } from "react-hot-toast";
+import { signIn } from "next-auth/react";
 
 const RegisterModal = () => {
   const loginModal = useLoginModal();
@@ -24,13 +27,27 @@ const RegisterModal = () => {
     try {
       setIsLoading(true);
       // TODO: ADD REGISTER LOG IN
+      await axios.post("/api/register", {
+        email,
+        password,
+        username,
+        name,
+      });
+
+      toast.success("Account created");
+      signIn("credentials", {
+        email,
+        password,
+      });
+
       registerModal.onClose();
     } catch (error) {
       console.log(error);
+      toast.error("Something went wrong");
     } finally {
       setIsLoading(false);
     }
-  }, [loginModal]);
+  }, [loginModal, email, password, username, name]);
 
   const bodyContent = (
     <div className="flex flex-col gap-4">
