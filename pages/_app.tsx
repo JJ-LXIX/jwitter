bb4b45104984f9d2bc51533760705fc591cacf25
Jagrath <jagrathshettylxix@gmail.com>
2023-03-28 11:41:26 +0530

    added session provider and Toaster

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,16 +4,19 @@ import Layout from "../Components/Layout";
 import Modal from "../Components/Modal";
 import LoginModal from "../Components/modals/LoginModal";
 import RegisterModal from "../Components/modals/RegisterModal";
+import { Toaster } from "react-hot-toast";
+import { SessionProvider } from "next-auth/react";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <SessionProvider session={pageProps.session}>
       {/* <Modal isOpen title="test modal" /> */}
+      <Toaster />
       <LoginModal />
       <RegisterModal />
       <Layout>
         <Component {...pageProps} />
       </Layout>
-    </>
+    </SessionProvider>
   );
 }
