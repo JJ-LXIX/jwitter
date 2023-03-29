eb6095d63721c81cc25ed2af50f457b2846b2e27
Jagrath <jagrathshettylxix@gmail.com>
2023-03-29 20:01:42 +0530

    added edit modal component to _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,7 @@ import Layout from "../Components/Layout";
 import Modal from "../Components/Modal";
 import LoginModal from "../Components/modals/LoginModal";
 import RegisterModal from "../Components/modals/RegisterModal";
+import EditModal from "../Components/modals/EditModal";
 import { Toaster } from "react-hot-toast";
 import { SessionProvider } from "next-auth/react";
 
@@ -13,6 +14,7 @@ export default function App({ Component, pageProps }: AppProps) {
       {/* <Modal isOpen title="test modal" /> */}
       <Toaster />
       <LoginModal />
+      <EditModal />
       <RegisterModal />
       <Layout>
         <Component {...pageProps} />
