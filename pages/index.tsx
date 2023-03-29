d361a21c8fcaa7bdb9199e6d8a541f69e4e8433f
Jagrath <jagrathshettylxix@gmail.com>
2023-03-29 21:17:48 +0530

    changed icon of website

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,7 +11,7 @@ export default function Home() {
         <title>Jwitter</title>
         <meta name="description" content="A fullstack twitter clone" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/LXIX.jpg" />
+        <link rel="icon" href="/goku.jpg" />
       </Head>
 
       <>
