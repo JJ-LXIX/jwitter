6ccd2a6182c39efc28dece1b1710439169537a43
Jagrath <jagrathshettylxix@gmail.com>
2023-03-31 20:54:45 +0530

    added pages for each post

---
+++ pages/posts/[postId].tsx
@@ -0,0 +1,37 @@
+import { useRouter } from "next/router";
+import React from "react";
+import usePost from "../../hooks/usePost";
+import { ClipLoader } from "react-spinners";
+import Header from "../../Components/Header";
+import PostItem from "../../Components/posts/PostItem";
+import Form from "../../Components/Form";
+
+type Props = {};
+
+function PostView({}: Props) {
+  const router = useRouter();
+  const { postId } = router.query;
+
+  const { data: fetchedPost, isLoading } = usePost(postId as string);
+
+  if (isLoading || !fetchedPost) {
+    return (
+      <div className="flex justify-center items-center h-full">
+        <ClipLoader color="lightblue" size={80} />
+      </div>
+    );
+  }
+  return (
+    <>
+      <Header label="Tweet" showBackArrow />
+      <PostItem data={fetchedPost} />
+      <Form
+        postId={postId as string}
+        isComment
+        placeholder="Tweet your reply"
+      />
+    </>
+  );
+}
+
+export default PostView;
