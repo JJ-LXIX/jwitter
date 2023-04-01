13a167807fb33e10df8958b97b3ed4a9310a0d75
Jagrath <jagrathshettylxix@gmail.com>
2023-04-01 11:48:45 +0530

    added api response limit config back

--- pages/api/posts/index.ts
+++ pages/api/posts/index.ts
@@ -2,11 +2,11 @@ import { NextApiRequest, NextApiResponse } from "next";
 import serverAuth from "../../../libs/serverAuth";
 import prisma from "../../../libs/prismadb";
 
-// export const config = {
-//   api: {
-//     responseLimit: "8mb",
-//   },
-// };
+export const config = {
+  api: {
+    responseLimit: false,
+  },
+};
 
 export default async function handler(
   req: NextApiRequest,
