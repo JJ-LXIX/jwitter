47defee568a811737288f85a1f503b7e4d5e4113
Jagrath <jagrathshettylxix@gmail.com>
2023-04-03 11:02:33 +0530

    changed tweet to login

--- Components/layout/SidebarTweetButton.tsx
+++ Components/layout/SidebarTweetButton.tsx
@@ -1,5 +1,5 @@
 import { useRouter } from "next/router";
-import { FaFeather } from "react-icons/fa";
+import { BiLogIn } from "react-icons/bi";
 import React, { useCallback } from "react";
 import useLoginModal from "../../hooks/useLoginModal";
 
@@ -16,11 +16,11 @@ const SidebarTweetButton = (props: Props) => {
   return (
     <div onClick={onClick}>
       <div className="mt-6 lg:hidden rounded-full h-14 w-14 p-4 flex items-center justify-center bg-sky-500 hover:bg-opacity-80 transition cursor-pointer">
-        <FaFeather size={24} color="white" />
+        <BiLogIn size={24} color="white" />
       </div>
       <div className="mt-6 hidden lg:block px-4 py-2 rounded-full bg-sky-500 hover:bg-opacity-90 cursor-pointer transition">
         <p className="hidden lg:block text-center font-semibold text-white text-[20px]">
-          Tweet
+          Login
         </p>
       </div>
     </div>
